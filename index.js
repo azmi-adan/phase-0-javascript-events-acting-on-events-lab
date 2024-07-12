f246c84d24872f602255d5dc0bb5c5f31b788859
azmi adan <azmi.adan2032@gmail.com>
2024-07-12 10:09:49 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+let dodger;
+
+function moveDodgerLeft() {
+  dodger = document.getElementById('dodger');
+  let left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  dodger = document.getElementById('dodger');
+  let left = parseInt(dodger.style.left);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
